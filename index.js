e8247d39096a29761539755d7effda5aab56fa3b
OsaJulian <osajulian@hotmail.com>
2019-11-23 01:49:29 -0500

    data structure for solution I

---
+++ index.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const chars = {};
+  for (let char of str)
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+    }
+  return chars;
+}
+
+module.exports = maxChar;
+
+console.log(maxChar('osa'));
